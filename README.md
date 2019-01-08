7b233874816ad2f893e9e72ef33183f1e82e2310
Andre <andre@dev-next.com>
2019-01-08 14:41:21 -0200

    :tada: First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Coin Exchange
+
+An project made as an assignment for a TDD course.
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './src/index.js',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
