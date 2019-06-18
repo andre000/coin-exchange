da92886288c3f161c892cec72a0abbb52ebc9ee9
André Adriano <andre@dev-next.com>
2019-06-18 10:31:14 -0300

    Changes due to package updates

--- src/components/interface/index.js
+++ src/components/interface/index.js
@@ -1,16 +1,10 @@
-import dotenv from 'dotenv';
 import { create } from 'axios';
-
-dotenv.config();
 
 export default (type) => {
   switch (type) {
     case 'CurrencyStack':
       return create({
         baseURL: 'https://api.currencystack.io/currency',
-        params: {
-          apikey: process.env.CURRENCYSTACK_API,
-        },
       });
     case 'Cryptonator':
       return create({
